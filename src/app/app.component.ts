22774c233b021b266a8fef4c2521f3fe32935478
Aissa Amina <aissa.amina.2014@gmail.com>
2018-06-14 13:34:33 +0200

    creation form and updateé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TOdo List ';
+
+  todos = [
+    
+         { 
+     label:'Salah Taouririt', done:false, priority:3 
+        },
+        { 
+          label:'Aissa amia', done:true, priority:1
+             },
+             { 
+              label:'Salah Taouririt', done:false, priority:4
+                 },
+
+
+
+
+
+         ];
+          addTodo(newTodolable){
+var newTodo ={
+label: newTodolable,
+priority:1,
+done:false
+
+};
+this.todos.push(newTodo);
+
+
+          }
+
+
+        
 }
